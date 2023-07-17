feecb1aa9d47532d10d827cf08e9ad76125c4bbb
Sh1n0u <florian.sune@gmail.com>
2023-07-17 21:39:08 +0200

    last commit test

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import data from '../../assets/logements.json';
-import './card.scss';
+import './card.scss'
 
 function Card() {
   return (
     <div className="card-container">
-      {data.map((item) => (
-        <Link key={item.id} to={`/content/${item.id}`} className="card">
-          <img src={item.cover} alt="Cover" className="card-img" />
-          <p>{item.title}</p>
-        </Link>
-      ))}
+        {data.map((item) => (
+            <Link key={item.id} to={`/content/${item.id}`} className="card">
+                <img src={item.cover} alt="Cover" className="card-img" />
+                <p>{item.title}</p>
+            </Link>
+        ))}
     </div>
   );
 }
